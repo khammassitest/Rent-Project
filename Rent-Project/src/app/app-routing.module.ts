f9b7381ab43bfbb005c42a5d648356a0daa07eff
Nader <naderkhammassi37@gmail.com>
2025-04-30 22:21:22 +0100

    Add new user page and Update logo and title in index.html

--- Rent-Project/src/app/app-routing.module.ts
+++ Rent-Project/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { RentalComponent } from './rental/rental.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   { path: 'rental', component:RentalComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'user', component: UserComponent}
 ];
 
 @NgModule({
